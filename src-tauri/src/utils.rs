5938e866cec44094081ec3314a26737c2ee3b7b9
CJ Pais <cj@cjpais.com>
2025-06-26 15:31:36 -0700

    fix?

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,18 +1,59 @@
 use enigo::Enigo;
+use enigo::Key;
 use enigo::Keyboard;
 use enigo::Settings;
+
 use tauri::image::Image;
 use tauri::tray::TrayIcon;
 use tauri::AppHandle;
 use tauri::Manager;
+use tauri_plugin_clipboard_manager::ClipboardExt;
 
-pub fn paste(text: String, _: AppHandle) -> Result<(), String> {
+fn send_paste() -> Result<(), String> {
+    // Determine the modifier key based on the OS
+    #[cfg(target_os = "macos")]
+    let modifier_key = Key::Meta; // Command key on macOS
+    #[cfg(not(target_os = "macos"))]
+    let modifier_key = Key::Control; // Control key on other systems
+
     let mut enigo = Enigo::new(&Settings::default())
         .map_err(|e| format!("Failed to initialize Enigo: {}", e))?;
 
+    // Press both keys
     enigo
-        .text(&text)
-        .map_err(|e| format!("Failed to paste text: {}", e))?;
+        .key(modifier_key, enigo::Direction::Press)
+        .map_err(|e| format!("Failed to press modifier key: {}", e))?;
+    enigo
+        .key(Key::Unicode('v'), enigo::Direction::Press)
+        .map_err(|e| format!("Failed to press V key: {}", e))?;
+
+    // Release both keys
+    enigo
+        .key(Key::Unicode('v'), enigo::Direction::Release)
+        .map_err(|e| format!("Failed to release V key: {}", e))?;
+    enigo
+        .key(modifier_key, enigo::Direction::Release)
+        .map_err(|e| format!("Failed to release modifier key: {}", e))?;
+
+    Ok(())
+}
+
+pub fn paste(text: String, app_handle: AppHandle) -> Result<(), String> {
+    let clipboard = app_handle.clipboard();
+
+    // get the current clipboard content
+    let clipboard_content = clipboard.read_text().unwrap_or_default();
+
+    clipboard
+        .write_text(&text)
+        .map_err(|e| format!("Failed to write to clipboard: {}", e))?;
+
+    send_paste()?;
+
+    // restore the clipboard
+    clipboard
+        .write_text(&clipboard_content)
+        .map_err(|e| format!("Failed to restore clipboard: {}", e))?;
 
     Ok(())
 }
