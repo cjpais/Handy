a7d7d314e2da0b09fc529128763fb51e10f477bd
Jason Walkow <jason.walkow@gmail.com>
2026-05-14 15:18:05 -0600

    Update app logo assets

--- src/components/icons/HandyTextLogo.tsx
+++ src/components/icons/HandyTextLogo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import unburdnIconLogo from "../../assets/unburdn-icon-logo.png";
 
 const HandyTextLogo = ({
   width,
@@ -11,14 +12,15 @@ const HandyTextLogo = ({
   className?: string;
 }) => {
   const { t } = useTranslation();
+  const resolvedHeight = height ?? (width ? Math.min(width, 64) : undefined);
 
   return (
-    <div
-      className={`font-semibold tracking-normal text-text ${className ?? ""}`}
-      style={{ width, height }}
-    >
-      {t("brand.name")}
-    </div>
+    <img
+      alt={t("brand.name")}
+      className={`block object-contain ${className ?? ""}`}
+      src={unburdnIconLogo}
+      style={{ width, height: resolvedHeight }}
+    />
   );
 };
 
